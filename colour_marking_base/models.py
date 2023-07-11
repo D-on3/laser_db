fcd7cf52ee84ee75c0fa5c99f84fb8974f6065b3
D-on3 <104354779+D-on3@users.noreply.github.com>
2023-07-11 18:03:36 +0300

    update via upload

--- colour_marking_base/models.py
+++ colour_marking_base/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Material(models.Model):
+    name = models.CharField(max_length=100)
+    # Add other fields related to the material
+
+    def __str__(self):
+        return self.name
+
+class ColorOutcome(models.Model):
+    name = models.CharField(max_length=100)
+    # Add other fields related to the color outcome
+
+    def __str__(self):
+        return self.name
+
+class LaserParameter(models.Model):
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    color_outcome = models.ForeignKey(ColorOutcome, on_delete=models.CASCADE)
+    # Add other fields related to the laser parameters
+
+    def __str__(self):
+        return f"{self.material} - {self.color_outcome}"
