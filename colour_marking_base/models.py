126ca2da450d30023018f9a56ee44dec3f549dad
D-on3 <dnedyalkov396@gmail.com>
2023-07-11 21:45:39 +0300

    adding setings.py

--- colour_marking_base/models.py
+++ colour_marking_base/models.py
@@ -13,7 +13,27 @@ class ColorOutcome(models.Model):
     def __str__(self):
         return self.name
 
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    year = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Machine(models.Model):
+    manufacturer = models.CharField(max_length=100)
+    model_machine = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.manufacturer + ' ' + self.model_machine
+
 class LaserParameter(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
     material = models.ForeignKey(Material, on_delete=models.CASCADE)
     color = models.ForeignKey(ColorOutcome, on_delete=models.CASCADE)
     power = models.FloatField()
