9328d9a90331b9be1b59836f9b9e326870056088
D-on3 <dnedyalkov396@gmail.com>
2023-07-11 21:22:41 +0300

    adding setings.py

--- colour_marking_base/views.py
+++ colour_marking_base/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 
 from .models import ColorOutcome, LaserParameter
-from forms import LaserParameterForm
+from .forms import LaserParameterForm
 
 @login_required(login_url='login')
 def index(request):
