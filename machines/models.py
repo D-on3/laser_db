a243fbd472931dcef9ed33a49db14bc6d2b11e57
D-on3 <dnedyalkov396@gmail.com>
2023-07-13 13:26:08 +0300

    adding the api

--- machines/models.py
+++ machines/models.py
@@ -3,7 +3,9 @@ from django.db import models
 class Machine(models.Model):
     manufacturer = models.CharField(max_length=100)
     model_machine = models.CharField(max_length=100)
-    type = models.CharField(max_length=100)
+    machine_type = models.CharField(max_length=100)
+    machine_link = models.URLField(blank=True)
+    # Add additional fields related to machine information
 
     def __str__(self):
         return f'{self.manufacturer} {self.model_machine}'
