53d5d8eda0ea3eb1c759fb2c492fa8157fca5182
D-on3 <dnedyalkov396@gmail.com>
2023-08-18 20:12:25 +0300

    update

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import LaserSource,LaserMarkingParameters,MetalMaterial,LaserColorMarking
 
 # Register your models here.
+
+
+admin.register(LaserSource)
+admin.register(LaserMarkingParameters)
+admin.register(MetalMaterial)
+admin.register(LaserColorMarking)
