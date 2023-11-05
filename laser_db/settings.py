2eaa6321d2b210668c33fa12d71cc5f91580a259
D-on3 <dnedyalkov396@gmail.com>
2023-11-05 20:03:13 +0200

    update

--- laser_db/settings.py
+++ laser_db/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ymnex2%n6limpu=rb*5#!w$tqqjauy39x6ifd0)v#f9d9=*+qf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 #
 ALLOWED_HOSTS = ['laserparams.pythonanywhere.com']
 # ALLOWED_HOSTS = []
