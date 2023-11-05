122e3ebfd77753a2821cae2a05fc831c1a3f6a9d
D-on3 <dnedyalkov396@gmail.com>
2023-11-05 19:45:31 +0200

    update

--- laser_db/settings.py
+++ laser_db/settings.py
@@ -154,15 +154,15 @@ WSGI_APPLICATION = 'laser_db.wsgi.application'
 #
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'laserparams$default',
